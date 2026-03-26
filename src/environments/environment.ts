6358bccc05742a44add0334c3e616a9b54b151e9
Manuel Rivera <manuelgarciarivera4@gmail.com>
2026-03-26 16:36:45 -0600

    Fix: URL del backend actualizada en el Front

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://fit-manager-backend-gb183crfy-planifys-projects-8087f027.vercel.app/' // La URL que te dio Vercel del backend
+};
