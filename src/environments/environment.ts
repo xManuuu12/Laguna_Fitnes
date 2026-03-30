f27a6007a31bebfe068cdc90c535806f178c850d
Manuel Rivera <manuelgarciarivera4@gmail.com>
2026-03-29 22:41:05 -0600

    POR FAVOR QUE SE CONECTE EL BACKEND PT2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fit-manager-backend.vercel.app/' // La URL que te dio Vercel del backend
+  apiUrl: 'https://fit-manager-backend.vercel.app/api/' // La URL que te dio Vercel del backend
   //apiUrl: 'http://localhost:5000/api' // LOCALHOST PARA PRUEBAS LOCALES
 };
