df9aa091ce494a323c079b25ff78db549a568e0e
Sergio <sergiodidierg@gmail.com>
2026-05-05 17:00:18 -0600

    fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -47,7 +47,7 @@ export class AuthService {
       sessionStorage.removeItem('user');
     }
     this.currentUserSubject.next(null);
-    this.router.navigate(['/login']);
+    this.router.navigate(['/']);
   }
 
   getToken(): string | null {
